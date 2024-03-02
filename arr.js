2cd50472e14bb04ca6647a503d02f3b7210fcb90
cerendn <cerendncer@outlook.com>
2024-03-02 13:09:15 +0300

    update

---
+++ arr.js
@@ -0,0 +1,174 @@
+const students = [
+    {
+        fullName: "Berkay Turna",
+        age: 25,
+        isDeleted: false,
+        score: 93,
+        isActive: true,
+        courses: ["JavaScript", "React", "Node.js"],
+        instructors: [
+            { fullName: "Orkun Durmaz", topics: ["JavaScript", "React"] },
+            { fullName: "Hicran Ertugral", topics: ["TypeScript", "Angular"] },
+        ],
+    },
+    {
+        fullName: "Cenk Grid Kaynak",
+        age: 32,
+        isDeleted: false,
+        score: 85,
+        isActive: true,
+        courses: ["JavaScript", "React Native", ".Net Core"],
+        instructors: [
+            { fullName: "Orkun Durmaz", topics: ["JavaScript", "React"] },
+            { fullName: "Korhan Kosece", topics: [".Net Core"] },
+        ],
+    },
+    {
+        fullName: "Ali Riza Taskiran",
+        age: 24,
+        isDeleted: false,
+        score: 95,
+        isActive: true,
+        courses: ["Java", "Golang"],
+        instructors: [
+            { fullName: "Akin Kaldiroglu", topics: ["Spring", "C++"] },
+            { fullName: "Buse Seker", topics: ["Problem Solving"] },
+        ],
+    },
+    {
+        fullName: "Hakan Berke Temur",
+        age: 24,
+        isDeleted: false,
+        score: 90,
+        isActive: true,
+        courses: ["Java", "Golang"],
+        instructors: [
+            { fullName: "Akin Kaldiroglu", topics: ["Spring", "C++"] },
+            { fullName: "Buse Seker", topics: ["Problem Solving"] },
+        ],
+    },
+    {
+        fullName: "Barkin Sayin",
+        age: 22,
+        isDeleted: false,
+        score: 92,
+        isActive: true,
+        courses: ["Java", "Golang"],
+        instructors: [
+            { fullName: "Akin Kaldiroglu", topics: ["Spring", "C++"] },
+            { fullName: "Buse Seker", topics: ["Problem Solving"] },
+        ],
+    },
+    {
+        fullName: "Mert Kirant",
+        age: 23,
+        isDeleted: false,
+        score: 80,
+        isActive: true,
+        courses: ["Expressjs", "Node.js"],
+        instructors: [
+            { fullName: "Orkun Durmaz", topics: ["JavaScript", "React"] },
+            { fullName: "Onder Tarım", topics: ["React Native"] },
+        ],
+    },
+    {
+        fullName: "Onur Ege Erkek",
+        age: 22,
+        isDeleted: false,
+        score: 88,
+        isActive: true,
+        courses: ["Expressjs", "Node.js"],
+        instructors: [
+            { fullName: "Orkun Durmaz", topics: ["JavaScript", "React"] },
+            { fullName: "Onder Tarım", topics: ["React Native"] },
+        ],
+    },
+    {
+        fullName: "Firat Can Tas",
+        age: 24,
+        isDeleted: false,
+        score: 60,
+        isActive: false,
+        courses: ["Expressjs", "Node.js", ".Net Core"],
+        instructors: [
+            { fullName: "Orkun Durmaz", topics: ["JavaScript", "React"] },
+            { fullName: "Onder Tarım", topics: ["React Native"] },
+        ],
+    },
+    {
+        fullName: "Ceren Dincer",
+        age: 27,
+        isDeleted: false,
+        score: 100,
+        isActive: true,
+        courses: ["JavaScript", "HTNL"],
+        instructors: [
+            { fullName: "Orkun Durmaz", topics: ["JavaScript", "React"] },
+            { fullName: "Onder Tarım", topics: ["React Native"] },
+        ],
+    },
+    {
+        fullName: "Baris Peker",
+        age: 38,
+        isDeleted: true,
+        score: 75,
+        isActive: false,
+        courses: ["JavaScript", "HTNL"],
+        instructors: [
+            { fullName: "Orkun Durmaz", topics: ["JavaScript", "React"] },
+            { fullName: "Onder Tarım", topics: ["React Native"] },
+        ],
+    },
+];
+const isScoreMin = students.some((student) => {
+    return student.score > 82
+});
+console.log(isScoreMin);
+
+const isScoreMax = students.some((student) => {
+    return student.score > 95
+});
+console.log(isScoreMax);
+//implicit return
+//const isScoreMin = students.some((student) => student.score > 82);
+
+const isActives = students.every((student) => student.isActive)
+console.log(isActives);
+
+const findCeren = students.findIndex((student) => student.fullName === "Ceren Dincer");
+console.log(findCeren);
+
+const findC = students.find((student) => student.fullName === "Ceren Dincer");
+console.log(findC);
+
+
+// const sortedStudentByAge = students.sort((age1,age2) => age1.age - age2.age);
+// console.log(sortedStudentByAge);
+
+const sortedScore = students.sort((score1, score2) => {
+    if (score1.fullName.toUpperCase() < score2.fullName.toUpperCase()) {
+        return -1;
+    } else if (score1.fullName.toUpperCase() > score2.fullName.toUpperCase()) {
+        return 1;
+    } else return 0;
+});
+console.log(sortedScore);
+
+const sortedName = students.sort((name1, name2) => name1.fullName.toLowerCase() > name2.fullName.toLowerCase() ? 1 : -1);
+console.log(sortedName);
+
+let reduceNumbers = [3, 5, 9, 7, 10, 23]
+let totalReduce = 5;
+reduceNumbers.forEach((element) => totalReduce += element);
+console.log(totalReduce);
+
+let total = reduceNumbers.reduce((acc,number) => acc + number, 5);
+console.log(total);
+
+const activeStudents = students.reduce((acc,currentStudent) => {
+    if(currentStudent.isActive){
+        acc.push(currentStudent.fullName)
+    }
+    return acc;
+},[])
+console.log(activeStudents);
--- array-methods.js
+++ array-methods.js
@@ -1,82 +1,82 @@
-//map()
-const arr = [1, 4, 10, 12];
-const newArr = arr.map(item => item * 2);
-console.log(newArr);
+// //map()
+// const arr = [1, 4, 10, 12];
+// const newArr = arr.map(item => item * 2);
+// console.log(newArr);
 
-//forEach()
-const moviesName = ["Yüzüklerin Efendisi", "Hobbit", "300 Spartalı", "The Notebook"]
-moviesName.forEach(movie => console.log(movie));
+// //forEach()
+// const moviesName = ["Yüzüklerin Efendisi", "Hobbit", "300 Spartalı", "The Notebook"]
+// moviesName.forEach(movie => console.log(movie));
 
-//filter()
-const words = ["banana", "apple", "orange", "grape"]
-const filteredWords = words.filter(word => word.length > 5);
-console.log(filteredWords);
+// //filter()
+// const words = ["banana", "apple", "orange", "grape"]
+// const filteredWords = words.filter(word => word.length > 5);
+// console.log(filteredWords);
 
-//some()
-const numbers = [1, 3, 5, 7, 9];
-const hasEvenNumber = numbers.some(num => num % 2 === 0);
-console.log(hasEvenNumber);
+// //some()
+// const numbers = [1, 3, 5, 7, 9];
+// const hasEvenNumber = numbers.some(num => num % 2 === 0);
+// console.log(hasEvenNumber);
 
-//every()
-const people = [
-    { name: "Gül", age: 30 },
-    { name: "Fatih", age: 25 },
-    { name: "Yılmaz", age: 40 }
-];
-const allAdults = people.every(person => person.age >= 20);
-console.log(allAdults);
+// //every()
+// const people = [
+//     { name: "Gül", age: 30 },
+//     { name: "Fatih", age: 25 },
+//     { name: "Yılmaz", age: 40 }
+// ];
+// const allAdults = people.every(person => person.age >= 20);
+// console.log(allAdults);
 
-//find()
-const flowers =[
-    {name:"Rose",color:"Red"},
-    {name:"Chamomile",color:"Yellow"},
-    {name:"Sunflowers",color:"Yellow"}
-];
-const yellowFlower = flowers.find(flowers => flowers.color === "Yellow");
-console.log(yellowFlower);
+// //find()
+// const flowers =[
+//     {name:"Rose",color:"Red"},
+//     {name:"Chamomile",color:"Yellow"},
+//     {name:"Sunflowers",color:"Yellow"}
+// ];
+// const yellowFlower = flowers.find(flowers => flowers.color === "Yellow");
+// console.log(yellowFlower);
 
-//findIndex()
-const candles=["Lavanta","Portakal","Tarçın","Gül","Vanilya","Sandal Ağacı","Paçuli"];
-const vanilyaIndex = candles.findIndex(name => name === "Vanilya");
-console.log(vanilyaIndex);
+// //findIndex()
+// const candles=["Lavanta","Portakal","Tarçın","Gül","Vanilya","Sandal Ağacı","Paçuli"];
+// const vanilyaIndex = candles.findIndex(name => name === "Vanilya");
+// console.log(vanilyaIndex);
 
-//sort()
-const nums = [7,15,14,10,12,9,6];
-nums.sort();
-console.log(nums);
+// //sort()
+// const nums = [7,15,14,10,12,9,6];
+// nums.sort();
+// console.log(nums);
 
-const weatherData = [
-    { city :"İstanbul", temperature: 14 },
-    { city :"İzmir", temperature: 18 },
-    { city :"Ankara", temperature: 10 }
-];
-weatherData.sort((city1,city2) => city2.temperature - city1.temperature);
-console.log("Günlük Sıcaklıklar" ,weatherData);
+// const weatherData = [
+//     { city :"İstanbul", temperature: 14 },
+//     { city :"İzmir", temperature: 18 },
+//     { city :"Ankara", temperature: 10 }
+// ];
+// weatherData.sort((city1,city2) => city2.temperature - city1.temperature);
+// console.log("Günlük Sıcaklıklar" ,weatherData);
 
-// reduce
-const sayilar = [9,8,7,6,5,4];
-const toplam = sayilar.reduce((accumulator,value) => accumulator + value);
-console.log(toplam);
+// // reduce
+// const sayilar = [9,8,7,6,5,4];
+// const toplam = sayilar.reduce((accumulator,value) => accumulator + value);
+// console.log(toplam);
 
-const nums1 =[100,20,70,2];
-const maxNums = nums1.reduce((max,element) => max > element ? max :element);
-const minNums = nums1.reduce((min,element) => min < element ? min:element);
-console.log(maxNums);
-console.log(minNums);
+// const nums1 =[100,20,70,2];
+// const maxNums = nums1.reduce((max,element) => max > element ? max :element);
+// const minNums = nums1.reduce((min,element) => min < element ? min:element);
+// console.log(maxNums);
+// console.log(minNums);
 
-const greetPerson = ["Hoşgeldiniz","Keyifli","Alışverişler","Dileriz."];
-const mergedText = greetPerson.reduce((value,argüman) => value + " " + argüman);
-console.log(mergedText);
+// const greetPerson = ["Hoşgeldiniz","Keyifli","Alışverişler","Dileriz."];
+// const mergedText = greetPerson.reduce((value,argüman) => value + " " + argüman);
+// console.log(mergedText);
 
-const items = ['elma', 'armut', 'kiraz', 'elma', 'üzüm', 'armut', 'elma'];
-const itemCounts = items.reduce((obj,param) => {
-    if(obj[param]) {
-        obj[param] =0;
-    }
-    obj[param]++;
-    return obj;
-}, {});
-console.log("Tekrar eden elemanlar" ,itemCounts);
+// const items = ['elma', 'armut', 'kiraz', 'elma', 'üzüm', 'armut', 'elma'];
+// const itemCounts = items.reduce((obj,param) => {
+//     if(obj[param]) {
+//         obj[param] =0;
+//     }
+//     obj[param]++;
+//     return obj;
+// }, {});
+// console.log("Tekrar eden elemanlar" ,itemCounts);
 
 
 
